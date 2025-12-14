33621547fe146fa43b3a69a0d30f365958c161e7
Mateusz <makiesto@gmail.com>
2025-12-14 23:22:09 +0100

    Added ./LibraryDbContext.cs

---
+++ Data/LibraryDbContext.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using LibraryManagement.Models;
+
+namespace LibraryManagement.Data
+{
+    public class LibraryDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<BookAuthor> BookAuthors { get; set; }
+        public DbSet<Loan> Loans { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Konfiguracja relacji Many-to-Many między Book i Author
+            modelBuilder.Entity<BookAuthor>()
+                .HasKey(ba => new { ba.BookId, ba.AuthorId });
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne(ba => ba.Book)
+                .WithMany(b => b.BookAuthors)
+                .HasForeignKey(ba => ba.BookId);
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne(ba => ba.Author)
+                .WithMany(a => a.BookAuthors)
+                .HasForeignKey(ba => ba.AuthorId);
+
+            // Konfiguracja relacji Loan
+            modelBuilder.Entity<Loan>()
+                .HasOne(l => l.Book)
+                .WithMany(b => b.Loans)
+                .HasForeignKey(l => l.BookId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Loan>()
+                .HasOne(l => l.User)
+                .WithMany(u => u.Loans)
+                .HasForeignKey(l => l.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Indeksy dla wydajności
+            modelBuilder.Entity<Book>()
+                .HasIndex(b => b.ISBN)
+                .IsUnique();
+
+            modelBuilder.Entity<Book>()
+                .HasIndex(b => b.Title);
+
+            modelBuilder.Entity<Author>()
+                .HasIndex(a => new { a.FirstName, a.LastName });
+        }
+    }
+}
--- obj/Debug/net10.0/LibraryManagement.AssemblyInfo.cs
+++ obj/Debug/net10.0/LibraryManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10418c7aa314b0480d4319dc2665bb72b6b6112e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d71d9aa8b342de6c9d705681ae05da01f67127df")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
