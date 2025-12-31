4a238fdf2de8d4badc88cc26c1c7c7a588603b93
Mateusz Stojek <makiesto@gmail.com>
2025-12-31 17:48:40 +0100

    Add Books controller with Index and Details views

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,48 @@
+﻿using LibraryManagement.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly LibraryDbContext _context;
+
+        public BooksController(LibraryDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Books
+        public async  Task<IActionResult> Index()
+        {
+            var books = await _context.Books
+                .Include(b => b.BookAuthors)
+                .ThenInclude(ba => ba.Author)
+                .ToListAsync();
+
+            return View(books);
+        }
+
+        // GET: Books/Details/1
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books
+                .Include(b => b.BookAuthors)
+                .ThenInclude(ba => ba.Author)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+    }
+}
