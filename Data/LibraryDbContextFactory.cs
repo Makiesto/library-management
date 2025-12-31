33bd064ab3ca96038d256c86c914499ce0103feb
Mateusz Stojek <makiesto@gmail.com>
2025-12-31 19:44:09 +0100

    Bugfix

---
+++ Data/LibraryDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace LibraryManagement.Data
+{
+    public class LibraryDbContextFactory
+        : IDesignTimeDbContextFactory<LibraryDbContext>
+    {
+        public LibraryDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<LibraryDbContext>();
+           
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LibraryDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+            
+            return new LibraryDbContext(optionsBuilder.Options);
+        }
+    }
+}
