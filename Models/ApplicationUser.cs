baad97959f8d96a6f90c7302248964cd771ce329
Mateusz Stojek <makiesto@gmail.com>
2026-01-08 09:41:10 +0100

    Add Identity packages and ApplicationUser model

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [StringLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+        public DateTime RegistrationDate { get; set; } = DateTime.Now;
+
+        public string FullName => $"{FirstName} {LastName}";
+    }
