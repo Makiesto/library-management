0aa6f85d2fc5b9b3b14eaa97e03f9f30fbd072ae
Mateusz Stojek <makiesto@gmail.com>
2026-01-08 10:02:15 +0100

    Add ViewModels for registration and login

---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+using namespace System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagement.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "First name is required.")]
+        [StringLength(100, ErrorMessage = "First name cannot be longer than 100 characters.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        [StringLength(100, ErrorMessage = "Last name cannot be longer than 100 characters.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters long.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required.")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
