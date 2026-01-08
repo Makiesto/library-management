125c992c0e67f91d6ed0fda4d2dde4bcb2268400
Mateusz Stojek <makiesto@gmail.com>
2026-01-08 22:22:47 +0100

    Added view model for book form

---
+++ ViewModels/BookFormViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace LibraryManagement.ViewModels
+{
+    public class BookFormViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(200)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "ISBN is required")]
+        [RegularExpression(@"^(?:\d{10}|\d{13})$", ErrorMessage = "ISBN must be 10 or 13 digits")]
+        public string ISBN { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1000, 2100)]
+        public int PublicationYear { get; set; }
+
+        [Required]
+        [Range(0, 1000)]
+        public int AvailableCopies { get; set; }
+
+        [StringLength(1000)]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Select at least one author")]
+        public List<int> SelectedAuthorIds { get; set; } = new List<int>();
+
+        public List<SelectListItem>? AvailableAuthors { get; set; }
+    }
+}
