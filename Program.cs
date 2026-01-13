248e330406e9f741678f0196d2222f30915d2ed3
Mateusz Stojek <142672873+Makiesto@users.noreply.github.com>
2026-01-14 00:39:37 +0100

    Fix formatting issue in Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<LibraryDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services
     .AddIdentity<ApplicationUser, IdentityRole>(options =>
