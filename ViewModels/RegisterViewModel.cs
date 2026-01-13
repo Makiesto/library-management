9434bcd488aff763f88056f42c32f8775a3b1649
Mateusz Stojek <makiesto@gmail.com>
2026-01-13 19:59:55 +0100

    Visualization of fields fix

--- ViewModels/RegisterViewModel.cs
+++ ViewModels/RegisterViewModel.cs
@@ -10,10 +10,12 @@ namespace LibraryManagement.ViewModels
 
         [Required]
         [StringLength(100)]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; } = string.Empty;
 
         [Required]
         [StringLength(100)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; } = string.Empty;
 
         [Required]
@@ -24,6 +26,7 @@ namespace LibraryManagement.ViewModels
         [Required]
         [DataType(DataType.Password)]
         [Compare("Password")]
+        [Display(Name = "Confirm Password")]
         public string ConfirmPassword { get; set; } = string.Empty;
     }
 }
