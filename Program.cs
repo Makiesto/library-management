d10db81ba289b640facd6491b7acc495c18066d5
Mateusz Stojek <142672873+Makiesto@users.noreply.github.com>
2026-01-14 16:44:42 +0100

    Fix formatting in Program.cs for DbContext setup

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<LibraryDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-                                               );
+    );
 
 builder.Services
     .AddIdentity<ApplicationUser, IdentityRole>(options =>
